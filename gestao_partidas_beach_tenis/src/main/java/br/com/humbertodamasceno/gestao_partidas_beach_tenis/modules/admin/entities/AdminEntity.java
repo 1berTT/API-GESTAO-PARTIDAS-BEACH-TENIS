6318392791f5276492f480908ab6558082f6df09
Humberto Cavalcante Damasceno <44322360+1berTT@users.noreply.github.com>
2025-11-25 17:29:01 -0300

    Configurando db postgres via docker e criando a tabela de admin

--- gestao_partidas_beach_tenis/src/main/java/br/com/humbertodamasceno/gestao_partidas_beach_tenis/modules/admin/entities/AdminEntity.java
+++ gestao_partidas_beach_tenis/src/main/java/br/com/humbertodamasceno/gestao_partidas_beach_tenis/modules/admin/entities/AdminEntity.java
@@ -1,17 +1,29 @@
 package br.com.humbertodamasceno.gestao_partidas_beach_tenis.modules.admin.entities;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "admin")
 public class AdminEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Length(min = 3, max = 255, message = "O nome deve ter entre 3 e 255 caracteres")
@@ -26,5 +38,11 @@ public class AdminEntity {
 
     @Length(min = 8, max = 255, message = "A senha deve ter entre 8 e 255 caracteres")
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }
